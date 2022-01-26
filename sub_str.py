e4c254379ef8b9b27b530d3d7187a23169099172
mitakazuya <17441@toba-cmt.ac.jp>
2022-01-26 16:45:07 +0900

    変更

--- sub_str.py
+++ sub_str.py
@@ -8,7 +8,7 @@ context = zmq.Context()
 
 #このクライアントは、ポート5556に接続します(バックグラウンドにて)
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://172.20.10.4:5556")
+socket.connect("tcp://10.40.1.43:5556")
 
 #チャネルの目盛りをあわせる
 socket.setsockopt_string(zmq.SUBSCRIBE,topic)
