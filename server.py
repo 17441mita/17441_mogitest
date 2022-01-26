5cdea28b2ef476b8081abd1855416a54f7ad7e98
mitakazuya <17441@toba-cmt.ac.jp>
2022-01-26 17:35:56 +0900

    5番変更

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 import zmq
+import sys
 
 def start_server():
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5556")
-    
     print("Server startup.")
-    
     while True:
         message = socket.recv_string()
         print("Received message = %s" % message)
-        socket.send_string("Reply: %s" % message)
-        
+
+        print("あなたの名前 サーバからクライアントへ送るメッセージ:", end='')
+        message = sys.stdin.readline()
+        socket.send_string(message.strip())
+
     socket.close()
     context.destroy()
-    
+
 if __name__ == "__main__":
-    start_server()
+    start_server() 
